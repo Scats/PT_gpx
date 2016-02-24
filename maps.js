03ea3558a8f20dfaa8b8992c9b50827d9e01ebf6
Scats <minet.simon@gmail.com>
2016-02-24 18:28:19 +0100

    Ajout des markers au début et à la fin du circuit

--- maps.js
+++ maps.js
@@ -1,6 +1,65 @@
+var mapOptions = {
+     mapTypeId: google.maps.MapTypeId.ROADMAP
+}
 
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-var map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: -34.397, lng: 150.644},
-  zoom: 8
+$.ajax({
+    url: "cartes/map.gpx",
+    type: "GET",
+    datatype: "xml",
+    success:function(xml){
+        
+        
+        var points = [];
+        var bounds = new google.maps.LatLngBounds(); //Définit les limites de la carte
+        var coordonneesMap;
+        var lat, long;
+        var test;
+        var lastCoordonnees;
+
+        $(xml).find("trkpt").each(function(){
+            lat = $(this).attr("lat");
+            lng = $(this).attr("lon");
+
+            coordonneesMap = new google.maps.LatLng(lat, lng);
+            
+            
+            
+            points.push(coordonneesMap);
+            bounds.extend(coordonneesMap);
+            
+        });
+        
+        var poly = new google.maps.Polyline({
+          path: points,
+          strokeColor: "#FF00AA",
+          strokeOpacity: .7,
+          strokeWeight: 4,
+        });
+        
+        //console.log(coordonneesMap);
+        
+        var marker_first = new google.maps.Marker({
+            icon: "http://maps.google.com/mapfiles/dd-start.png",
+            position: points[points.length-1],
+            map: map,
+        });
+        
+        marker_first.setMap(map);
+        
+        var marker_end = new google.maps.Marker({
+            icon: "http://maps.google.com/mapfiles/dd-end.png",
+            position: points[0],
+            map: map,
+        });
+        
+        marker_end.setMap(map);
+        
+        
+        console.log(coordonneesMap.lat());
+        
+        poly.setMap(map);
+        map.fitBounds(bounds);}
+    
 });
