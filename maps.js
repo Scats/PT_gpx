4a7d7a152e4378dd8deed122423dc8f93ce3ee6d
Scats <minet.simon@gmail.com>
2016-02-28 18:19:10 +0100

    Ajout des lignes afin de trouver l'altitude minimum et maximum

--- maps.js
+++ maps.js
@@ -5,7 +5,7 @@ var mapOptions = {
 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
 $.ajax({
-    url: "cartes/map.gpx",
+    url: "cartes/breche.gpx",
     type: "GET",
     datatype: "xml",
     success:function(xml){
@@ -15,16 +15,13 @@ $.ajax({
         var bounds = new google.maps.LatLngBounds(); //Définit les limites de la carte
         var coordonneesMap;
         var lat, long;
-        var test;
-        var lastCoordonnees;
+        var elevation, elevationFinalMax = 0, elevationFinalMin=10000;
 
         $(xml).find("trkpt").each(function(){
             lat = $(this).attr("lat");
             lng = $(this).attr("lon");
 
             coordonneesMap = new google.maps.LatLng(lat, lng);
-            
-            
             
             points.push(coordonneesMap);
             bounds.extend(coordonneesMap);
@@ -38,26 +35,51 @@ $.ajax({
           strokeWeight: 4,
         });
         
-        //console.log(coordonneesMap);
         
-        var marker_first = new google.maps.Marker({
+       /** INSERTION DES DEUX MARKES **/
+        
+        var marker_start = new google.maps.Marker({
             icon: "http://maps.google.com/mapfiles/dd-start.png",
-            position: points[points.length-1],
-            map: map,
-        });
-        
-        marker_first.setMap(map);
-        
-        var marker_end = new google.maps.Marker({
-            icon: "http://maps.google.com/mapfiles/dd-end.png",
             position: points[0],
             map: map,
         });
         
-        marker_end.setMap(map);
+        
+        var marker_end = new google.maps.Marker({
+            icon: "http://maps.google.com/mapfiles/dd-end.png",
+            position: points[points.length-1],
+            map: map,
+        });
+        
+        /** END MARKERS **/
+        
+        function auCentieme(nombre){
+            return Math.round(100*nombre)/100;
+        }
+        
+        $(xml).find("ele").each(function(){
+            elevation = $(this).text();
+            
+            if(elevation <= elevationFinalMin)
+                {
+                    elevationFinalMin = elevation;
+                }
+                        
+            if(elevation >= elevationFinalMax)
+                {
+                    elevationFinalMax = elevation;
+                }
+            
+            Math.rou
+            
+        });
+
+        $("#altMax").append(auCentieme(elevationFinalMax) + " m");
+        $("#altMin").append(auCentieme(elevationFinalMin) + " m");
         
         
-        console.log(coordonneesMap.lat());
+         console.log(elevationFinalMin);
+        
         
         poly.setMap(map);
         map.fitBounds(bounds);}
