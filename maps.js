ed4449e6f12ffd560da280e9445cdd978d92e0ce
Scats <minet.simon@gmail.com>
2016-04-13 18:15:30 +0200

    Ajout d'un tableau graphique afin de voir le déniveler. Factorisation de l'ancien code

--- maps.js
+++ maps.js
@@ -1,72 +1,140 @@
-var mapOptions = {
-     mapTypeId: google.maps.MapTypeId.ROADMAP
-}
-
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+google.load('visualization', '1', {packages: ['columnchart']});
 
 $.ajax({
-    url: "cartes/breche.gpx",
+    url: "cartes/map.gpx",
     type: "GET",
     datatype: "xml",
     success:function(xml){
         
-        
-        var points = [];
-        var bounds = new google.maps.LatLngBounds(); //Définit les limites de la carte
-        var coordonneesMap;
-        var lat, long;
-        var elevation, elevationFinalMax = 0, elevationFinalMin=10000;
-        var myDate;
-
+        var lat = new Array();
+		var lng = new Array();
+		var coordonneesMap = new Array();
+		var samples = 400;
+		var i = 0;
+		var distance = 0;
+		var distances = [];
+		
         $(xml).find("trkpt").each(function(){
-            lat = $(this).attr("lat");
-            lng = $(this).attr("lon");
-
-            coordonneesMap = new google.maps.LatLng(lat, lng);
-            
-            points.push(coordonneesMap);
-            bounds.extend(coordonneesMap);
-            
+            lat[i] = parseFloat($(this).attr("lat"));
+            lng[i] = parseFloat($(this).attr("lon"));
+			
+			i++; 
         });
         
-        var poly = new google.maps.Polyline({
-          path: points,
+        var maxPath = Math.round(lat.length / samples);
+		var path = [];
+		
+		if ( maxPath > 1)
+		{
+			for(var i in lat)
+			{
+				if(( i %maxPath ) == 0) path.push({'lat':lat[i],'lng':lng[i]});
+			}
+		}else
+		{
+			path.push({'lat':lat[i],'lng':lng[i]});
+		}
+		
+		console.log("MaxPath: " + maxPath);
+		
+		var elevator = new google.maps.ElevationService;
+		
+		var map = new google.maps.Map(document.getElementById('map'), {
+    		zoom: 11,
+    		center: path[1],
+    		mapTypeId: 'terrain'
+  		});
+		
+		var poly = new google.maps.Polyline({
+          path: path,
           strokeColor: "#FF00AA",
           strokeOpacity: .7,
           strokeWeight: 4,
+		  map: map
         });
-                
-        /** RECUPERATION DE LA LONGUEUR DU TRACE **/
-        
+		
+		elevator.getElevationAlongPath({
+    		'path': path,
+    		'samples': samples
+  		}, plotElevation);
+		
+		function plotElevation(elevations, status) {
+		
+		var mousemarker = null;
+		
+	  var chartDiv = document.getElementById('elevation_chart');
+	  if (status !== google.maps.ElevationStatus.OK) {
+		// Show the error code inside the chartDiv.
+		chartDiv.innerHTML = 'Cannot show elevation: request failed because ' +
+			status;
+		return;
+	  }
+	  // Create a new chart in the elevation_chart DIV.
+	  var chart = new google.visualization.ColumnChart(chartDiv);
+
+	  // Extract the data from which to populate the chart.
+	  // Because the samples are equidistant, the 'Sample'
+	  // column here does double duty as distance along the
+	  // X axis.
+	  var data = new google.visualization.DataTable();
+	  data.addColumn('string', 'Sample');
+	  data.addColumn('number', 'Altitude');
+	  for (var i = 0; i < elevations.length; i++) {
+		  
+		  		var dist = (distance/1000).toFixed(1) ;
+				var dist_z = (distance/1000).toFixed(0) ;
+				if ( dist == dist_z ) dist = dist_z ;
+				data.addRow([''+dist + ' Km', elevations[i].elevation]);
+				if ( i < elevations.length - 1 )
+				{
+					distances[i] = google.maps.geometry.spherical.computeDistanceBetween (elevations[i].location, elevations[i+1].location);
+					distance = distance + distances[i];
+				}
+	  }
+
+	  // Draw the chart using the data within its DIV.
+	  chart.draw(data, {
+		height: 160,
+		legend: 'none',
+	  });
+		
+	  google.visualization.events.addListener(chart, 'onmouseover', function(e)
+		{
+			if (mousemarker == null)
+			{
+				data.addRow(['', elevations[e.row].elevation])
+				mousemarker = new google.maps.Marker({
+				position: elevations[e.row].location,
+				map: map,
+				icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+				});
+			} else {
+				mousemarker.setPosition(elevations[e.row].location);
+			}
+		});
+	}
+     /*             
         var longueur = google.maps.geometry.spherical.computeLength(points);
-        console.log("La longuer est de " + longueur);
+        console.log("La longuer est de " + longueur);*/
         
        /** INSERTION DES DEUX MARKES **/
         
         var marker_start = new google.maps.Marker({
             icon: "http://maps.google.com/mapfiles/dd-start.png",
-            position: points[0],
+            position: path[0],
             map: map,
         });
         
         
         var marker_end = new google.maps.Marker({
             icon: "http://maps.google.com/mapfiles/dd-end.png",
-            position: points[points.length-1],
+            position: path[path.length-1],
             map: map,
         });
                 
         /** END MARKERS **/
         
-        function auCentieme(nombre){
-            return Math.round(100*nombre)/100;
-        }
-        
-        function auKilometre(nombre){
-            return Math.round(nombre);
-        }
-        
-        $(xml).find("ele").each(function(){
+        /*$(xml).find("ele").each(function(){
             elevation = $(this).text();
             
             if(elevation <= elevationFinalMin)
@@ -87,23 +155,15 @@ $.ajax({
         
          console.log(elevationFinalMin);
         
-        /*$(xml).find("time").each(function(){
+        $(xml).find("time").each(function(){
             
             
             console.log($(this));
         });*/
-        
-        
-        
-        
+		
+		
         //myDate = new Date('2016-01-08T08:57:35Z'); //ISO-8601
         //console.log(myDate.getTimezoneOffset());
-        
-        poly.setMap(map);
-        map.fitBounds(bounds);
     }
-  
-       
-    
-    
+	
 });
